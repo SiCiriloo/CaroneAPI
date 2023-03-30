fc2d3d84663ed95deed99a379a037db8f4ee4a88
SiCiriloo <39436808+SiCiriloo@users.noreply.github.com>
2023-03-30 12:22:03 -0300

    Add files via upload

---
+++ caroneapi.js
@@ -0,0 +1,184 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const fileUpload = require('express-fileupload');
+const port = 8000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const fs = require('fs');
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+extended: true
+}));
+app.use(fileUpload({
+debug: true
+}));
+app.use("/", express.static(__dirname + "/"))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
+
+const dirBot = './bot'
+
+if (!fs.existsSync(dirBot)){
+  fs.mkdirSync(dirBot)
+}
+
+const client = new Client({
+  authStrategy: new LocalAuth({ clientId: 'bot-zdg-shop' }),
+  puppeteer: { headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ] }
+});
+
+client.initialize();
+
+io.on('connection', function(socket) {
+  socket.emit('message', '© BOT-ZDG - Iniciado');
+  socket.emit('qr', './icon.svg');
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', '© BOT-ZDG QRCode recebido, aponte a câmera  seu celular!');
+    });
+});
+
+client.on('ready', () => {
+    socket.emit('ready', '© BOT-ZDG Dispositivo pronto!');
+    socket.emit('message', '© BOT-ZDG Dispositivo pronto!');
+    socket.emit('qr', './check.svg')	
+    console.log('© BOT-ZDG Dispositivo pronto');
+});
+
+client.on('authenticated', () => {
+    socket.emit('authenticated', '© BOT-ZDG Autenticado!');
+    socket.emit('message', '© BOT-ZDG Autenticado!');
+    console.log('© BOT-ZDG Autenticado');
+});
+
+client.on('auth_failure', function() {
+    socket.emit('message', '© BOT-ZDG Falha na autenticação, reiniciando...');
+    console.error('© BOT-ZDG Falha na autenticação');
+});
+
+client.on('change_state', state => {
+  console.log('© BOT-ZDG Status de conexão: ', state );
+});
+
+client.on('disconnected', (reason) => {
+  socket.emit('message', '© BOT-ZDG Cliente desconectado!');
+  console.log('© BOT-ZDG Cliente desconectado', reason);
+  client.initialize();
+});
+});
+
+// POST send-message
+app.post('/send-message', [
+  body('number').notEmpty(),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = req.body.number.replace(/\D/g,'');
+  const message = req.body.message;
+  const numberDDI = number.substr(0, 2);
+  const numberDDD = number.substr(2, 2);
+  const numberUser = number.substr(-8, 8);
+
+  console.log(number)
+
+  if (numberDDI !== "55") {
+    const numberZDG = number + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+  else if (numberDDI === "55" && parseInt(numberDDD) <= 30) {
+    const numberZDG = "55" + numberDDD + "9" + numberUser + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+  else if (numberDDI === "55" && parseInt(numberDDD) > 30) {
+    const numberZDG = "55" + numberDDD + numberUser + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+});
+
+
+client.on('message', async msg => {
+
+  if (msg.body !== "") {
+    msg.reply('Acesse a Comunidade ZDG clincando no link http://comunidadezdg.com.br/ e aprenda como criar automações e robôs para WhatsApp mesmo que vcoê não seja programador.')
+  }
+
+});
+
+console.log("\nA Comunidade ZDG é a oportunidade perfeita para você aprender a criar soluções incríveis usando as APIs, sem precisar de experiência prévia com programação. Com conteúdo exclusivo e atualizado, você terá tudo o que precisa para criar robôs, sistemas de atendimento e automações do zero. O curso é projetado para iniciantes e avançados, e oferece um aprendizado prático e passo a passo para que você possa criar soluções incríveis.")
+console.log("\nIncreva-se agora acessando link: comunidadezdg.com.br\n")
+
+server.listen(port, function() {
+        console.log('App running on *: ' + port);
+});
